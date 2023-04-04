aa62929145236e2b362d4d1b56c47bf6afa268a6
Павел Афанасьев <makafanasiev@gmail.com>
2023-04-04 19:42:13 +0300

    <requred>

--- RISK_ASSESSMENT/RISK_ASSESSMENT/App_data.py
+++ RISK_ASSESSMENT/RISK_ASSESSMENT/App_data.py
@@ -1,4 +1,8 @@
+# -*- coding: cp1251 -*- 
 import tkinter as tk
+from turtle import window_width
+from itertools import product
+
 
 class MatrixApp:
     def __init__(self, master):
@@ -9,7 +13,7 @@ class MatrixApp:
         self.create_widgets()
         
     def create_widgets(self):
-        self.matrix_frame = tk.Frame(self.master)
+        self.matrix_frame = tk.Frame(self.master,width=10,height=5)
         self.matrix_frame.pack(anchor="nw")
         
         for i in range(self.num_rows):
@@ -18,25 +22,27 @@ class MatrixApp:
                 self.matrix[i][j].grid(row=i, column=j)
         
         self.row_spinbox_label = tk.Label(self.master, text="Rows:")
-        self.row_spinbox = tk.Spinbox(self.master, from_=1, to=6, command=self.update_matrix_size)
+        self.row_spinbox = tk.Spinbox(self.master, from_=3, to=6, command=self.update_matrix_size)
         self.col_spinbox_label = tk.Label(self.master, text="Cols:")
-        self.col_spinbox = tk.Spinbox(self.master, from_=1, to=6, command=self.update_matrix_size)
+        self.col_spinbox = tk.Spinbox(self.master, from_=3, to=6, command=self.update_matrix_size)
         
-        self.row_spinbox_label.pack(side=tk.TOP,anchor="nw")
-        self.row_spinbox.pack(side=tk.TOP,anchor="nw")
-        self.col_spinbox_label.pack(side=tk.TOP,anchor="nw")
-        self.col_spinbox.pack(side=tk.TOP,anchor="nw")
+        self.row_spinbox_label.pack(side=tk.BOTTOM,anchor="nw")
+        self.row_spinbox.pack(side=tk.BOTTOM,anchor="nw")
+        self.col_spinbox_label.pack(side=tk.BOTTOM,anchor="nw")
+        self.col_spinbox.pack(side=tk.BOTTOM,anchor="nw")
         
         
         self.reset_button = tk.Button(self.master, text="Reset", command=self.reset_matrix)
         self.reset_button.pack(side=tk.BOTTOM,anchor="nw")
+        self.get_button = tk.Button(self.master, text="Get", command=self.do_some_magic )
+        self.get_button.pack(side=tk.BOTTOM,anchor="nw")
         
     def update_matrix_size(self):
         new_rows = int(self.row_spinbox.get())
         new_cols = int(self.col_spinbox.get())
         
         
-        if new_rows > 6 or new_cols > 6:
+        if new_rows > 6 or new_cols > 6 or new_rows <3 or new_cols < 3:
             return
         
         # добавляем или удаляем строки
@@ -85,11 +91,48 @@ class MatrixApp:
                     except ValueError:
                         pass 
             data.append(row_data)
-        print(data)
+        
+        return(data)
+    
+
+    def all_combinations(self):
+        arr = self.get_matrix_data ()
+        rows = len(arr)
+        cols = len(arr[0])
+        print ([list(comb) for comb in product(*arr)])
+        return [list(comb) for comb in product(*arr)]
+    def multiplicate_matrix(self):
+        matrix = self.all_combinations()
+        row_products = []
+        for row in matrix:
+            row_product = 1
+            for num in row:
+                row_product *= num
+            row_products.append(row_product)
+        print("*",row_products)
+        return row_products
+    def plus_matrix(self):
+        matrix = self.all_combinations()
+        row_products = []
+        for row in matrix:
+            row_product = 0
+            for num in row:
+                row_product += num
+            row_products.append(row_product)
+        print("+",row_products)
+        return row_products
+    def do_some_magic(self):
+        self.multiplicate_matrix()
+        self.plus_matrix()
+        
+
+
+    
+    
 class App_data:
     
   
-    def _launch_win() :
+    def _launch_win() :                                               
          
         import tkinter as tk
 
@@ -102,7 +145,7 @@ class App_data:
         root.iconphoto(False,photo)
 
         root.title("RISK_ASSESSMENT")
-        root.geometry('600x400+200+100')
+        root.geometry('600x250+200+100')
         app = MatrixApp(root)
 
         
